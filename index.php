8ee0db622c077fbaebe688e67692456cd0391388
alieushuf3 <alieushuf@gmail.com>
2024-06-30 01:31:04 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<?php
+echo "The version of the current PHP version is: " . phpversion();
+$x = 145;
+$y = 40;
+echo "<br><br><br><br>The sum of x and y is: " . ($x + $y);
+?>
+</body>
+</html>
